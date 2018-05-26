1c10893f908816071c4c2d09d252330d94feeb17
Noemi Romero <nlll02@yahoo.com>
2018-05-26 12:48:28 -0500

    installed gems and created the base for application controller

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'sinatra-activerecord'
+gem 'json'
+gem 'pry'
+gem 'bcrypt'
+gem 'pg', '0.21' 
+gem 'sinatra-cross_origin'
+
+
+
---
+++ controllers/ApplicationController.rb
@@ -0,0 +1,41 @@
+class ApplicationController < Sinatra::Base
+	require 'bundler'
+	Bundler.require()
+
+	ActiveRecord::Base.establish_connection(
+		:adapter => 'postgresql'
+		:database => 'food-pantry'
+
+		)
+	register Sinatr:CrossOrigin 
+
+		configure do 
+			enable :cross_origin
+		end
+
+		set :allow_origin, :any
+		set :allow_methods, [:get, :post, :options, :put, :patch, :delete]
+		set :allow_credentials, true 
+
+
+		options '*' do 
+			response.headers['Allow'] = 'HEAD, GET, POST, PUT, PATCH, DELETE'
+			response.headers['Access-Control-Allow-Origin'] = '*'
+			response.headers['Access-Control-Allow-Headers'] = 'X-Request-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept'
+		end
+
+
+		get '/' do 
+			{
+				success: false, 
+				message: 'Please consult the API documentation'
+			}.to_json
+		end
+
+
+		not_found do 
+			halt 404
+		end
+
+
+	end
